cf59bb690b64791d1c9f358296fe8320d88d1e45
David Salazar <dsalazar@iomediums.com>
2015-11-28 20:17:32 -0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dsalazar@iomediums.com'
 license          'All rights reserved'
 description      'Installs/Configures consul'
 long_description 'Installs/Configures consul'
-version          '0.1.0'
+version          '0.1.1'
 
 depends "runit", "~> 1.7.4"
